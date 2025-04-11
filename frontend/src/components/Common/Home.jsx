175bc1f13d2ac662e282c54d9d3b5b24583b9a78
makbulhosen5000 <makbulhosen5000@gmail.com>
2025-04-11 17:45:41 +0600

    home page and shop page updated

---
+++ frontend/src/components/Common/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+
+export default function Home() {
+  return (
+    <>
+    <Helmet>
+      <title>Ecom|Home</title>
+    </Helmet>
+    <div>Home</div>
+    </>
+  )
+}
--- frontend/src/components/Common/Shop.jsx
+++ frontend/src/components/Common/Shop.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Shop() {
   return (
-    <div>Shop</div>
+    <>
+       <Helmet>
+          <title>Ecom|Shop</title>
+        </Helmet>
+    </>
   )
 }
