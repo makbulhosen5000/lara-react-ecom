739d30d34c68fecb026cb5e11ea9ea8bb764bf3a
makbulhosen5000 <makbulhosen5000@gmail.com>
2025-09-06 19:03:34 +0600

    update cart context

--- frontend/src/components/Http.jsx
+++ frontend/src/components/Http.jsx
@@ -5,12 +5,12 @@ export const adminToken = () => {
     // This function retrieves the admin token from local storage
     // and returns it for use in API requests.
     const data = JSON.parse(localStorage.getItem("adminInfo"));
-    return data.token;
+    return data?.token;
 }
 export const userToken = () => {
 
     // This function retrieves the user token from local storage
     // and returns it for use in API requests.
     const data = JSON.parse(localStorage.getItem("userInfo"));
-    return data.token;
+    return data?.token;
 }
